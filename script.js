2a304bb0fe46b5327bdd0317ac20b6f73d5172dd
Dlol <dravennadas@gmail.com>
2021-12-09 21:13:24 -0600

    Added some code QoL stuff (classes, more comments, etc)

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Card class to keep it consistent
+class Card {
+	/**
+	 * Creates a card
+	 * @param {number} index The index of the card in the array
+	 * @param {number} rank The rank of the card (numeric only!!)
+	 * @param {string} type The type of the card (Spades, Diamonds etc.)
+	 * @param {string} img The relative path to the image
+	 */
+	constructor(index, rank, type, img){
+		this.indx = index;
+		this.cardRank = rank;
+		this.cardType = type;
+		this.img = img;
+	}
+}
+
 /**
  * Generates a random integer between `min` and `max` (inclusive)
  * @param {number} min Minimum
@@ -9,6 +26,7 @@ function getRandInt(min, max) {
 }
 
 // Keeps score in an object
+// (no class for this and typeCounter due to the fact that there is only one!)
 let score = {
 	player: 0,
 	computer: 0,
@@ -21,18 +39,6 @@ let typeCounter = {
 	Hearts: 0,
 	Diamonds: 0,
 	Clubs: 0,
-};
-
-// THIS IS AN EXAMPLE CARD
-let cardDef = {
-	// the index of the element (useful for retrieving the number)
-	indx: 0,
-	// the rank for the card itself (eg 2, 3, ...queen, king etc)
-	cardRank: 2,
-	// the type of the card (hearts, spades, whatever)
-	cardType: "Spades",
-	// the path to the image
-	img: "path/to/image.png",
 };
 
 // cards
@@ -54,13 +60,8 @@ function createCards() {
 	let cardTypes = ["Spades", "Hearts", "Diamonds", "Clubs"];
 	// Loops through and generates the cards using the example from cardDef
 	for (let index = 0; index < cardsToGen; index++) {
-		let outCard = {};
-
-		outCard.indx = index;
-		outCard.cardRank = (index % amtOfCards) + 2;
-		outCard.cardType = cardTypes[Math.floor(index / amtOfCards)];
-		outCard.img = `Deck of Cards Images/${index}.jpg`;
-
+		// Using a class constructor for ease of use 
+		let outCard = new Card(index, (index % amtOfCards) + 2, cardTypes[Math.floor(index / amtOfCards)],`Deck of Cards Images/${index}.jpg`);
 		cards.push(outCard);
 	}
 	// fancy formatting
@@ -70,24 +71,30 @@ function createCards() {
 createCards();
 
 /**
- *
- *
+ * Goes through all the steps to play a round
  */
 function playRound() {
+	// generate the cards
 	let playerCard = cards[getRandInt(0, 51)];
 	let computerCard = cards[getRandInt(0, 51)];
+	// make sure they aren't the same!
 	while (playerCard == computerCard) {
 		computerCard = cards[getRandInt(0, 51)];
 	}
+	// fancy output
 	console.table([playerCard, computerCard]);
 
+	// update images
 	document.getElementById("pCard").src = playerCard.img;
 	document.getElementById("cCard").src = computerCard.img;
 
+	// increment the variable for whatever card type you use
+	// (this is why i have the cardType property on the cards!)
 	typeCounter[playerCard.cardType]++;
 	typeCounter[computerCard.cardType]++;
 	score.total++;
 
+	// checks if you win or lose and does stuff accordingly
 	if (playerCard.cardRank > computerCard.cardRank) {
 		score.player++;
 		document.getElementById("outcome").innerText = "You won!";
@@ -102,6 +109,8 @@ function playRound() {
 		console.log(score);
 		return;
 	}
+	// ONLY runs if you dont win or lose
+	// (thanks return; statement!)
 	score.tie++;
 	document.getElementById("outcome").innerText = "You tied!";
 	alert("You tied!");
@@ -109,7 +118,11 @@ function playRound() {
 	console.log(score);
 }
 
+/**
+ * Does all the necessary updating for the score
+ */
 function displayScore() {
+	// Selects the element with the score (the <h1>)
 	document.getElementById("playerStat").children[1].innerText = score.player;
 	document.getElementById("computerStat").children[1].innerText =
 		score.computer;
@@ -117,26 +130,37 @@ function displayScore() {
 	document.getElementById("totalStat").children[1].innerText = score.total;
 }
 
+/**
+ * Restarts the game with a confirm box
+ */
 function restart() {
 	if (confirm("are you sure you wanna restart")) {
+		// resets the scores back to 0
 		score = {
 			player: 0,
 			computer: 0,
 			tie: 0,
 			total: 0,
 		};
+		// updates the stats
 		displayScore();
+		// shows the backs of the cards again
 		document.getElementById("pCard").src = "Deck of Cards Images/back.jpg";
 		document.getElementById("cCard").src = "Deck of Cards Images/back.jpg";
+		// updates the output
 		document.getElementById("outcome").innerText = "Restarted";
 	}
 }
 
+/**
+ * Draws two of the same card and declaes it a tie
+ */
 function pacifist() {
 	let card = cards[getRandInt(0, 51)];
 	score.total++;
+	// no need for checking if it's a tie or not!
 	score.tie++;
-	typeCounter[card.cardType]++;
+	typeCounter[card.cardType]+=2;
 	document.getElementById("pCard").src = card.img;
 	document.getElementById("cCard").src = card.img;
 	alert("You tied!");
@@ -144,6 +168,7 @@ function pacifist() {
 	displayScore();
 }
 
+// Opens and closes the help box
 function showHelp(open = true) {
 	if (open) {
 		document.querySelector(".helpPop").classList.remove("hidden");
@@ -152,6 +177,7 @@ function showHelp(open = true) {
 	document.querySelector(".helpPop").classList.add("hidden");
 }
 
+// Shows the stats of the amount of cards and the percent they take up
 function coolStats() {
 	let percentTypes = {
 		Spades: (typeCounter.Spades / score.total / 2) * 100,
@@ -159,8 +185,6 @@ function coolStats() {
 		Diamonds: (typeCounter.Diamonds / score.total / 2) * 100,
 		Clubs: (typeCounter.Clubs / score.total / 2) * 100,
 	};
-	// window.alert("g");
-	console.log("aa");
 	alert(`Type | Count | Percentage
 Spades   | ${typeCounter.Spades} | ${percentTypes.Spades}
 Hearts   | ${typeCounter.Hearts} | ${percentTypes.Hearts}
